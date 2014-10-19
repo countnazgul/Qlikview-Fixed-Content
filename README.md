5c289e7494ce24fb4e8e1c0f091f9de0b77e8dbb
Stefan Stoichev <stefan.stoichev@gmail.com>
2014-10-19 10:58:13 +0100

    * Updated README.md * Updated .qar * Script.js - position now is including and #QvAjaxToolbar

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
 Qlikview-Fixed-Content
 ======================
 
-Qlikview Extension (for v11.20) which change the style of objects which have "_fixed" as pard of their ObjectId (for example  TX101_fixed). 
-
-For this objects "positions: fixed" is added when the app is displayed in browser (will not work with IE Plugin)
-
-Using this extension will help making fixed headers so the selections part(s) will be always visible and available for users to change them. 
-
-macro_helper.txt contain two vbscript subs which can help developers adding _fixed to the objects. Just select all the objects that you want and execute "ChangeId_AddFixed" sub. 
-
-"ChangeId_RemoveFixed" will remove "_fixed" from the selected/active objects ids
-
-Don't forget to add the extension in the app.
-
-Need to be added and fixed position and for top ajax header. 
-
-(Screenshots and video will be added soon)
-
+Create objects with fixed position in Qlikview (11,11.20) AJAX client
--- Script.js
+++ Script.js
@@ -1,31 +1,36 @@
-//Qva.LoadScript('https://getfirebug.com/firebug-lite-beta.js#startOpened=true', function () {
+// Qva.LoadScript('https://getfirebug.com/firebug-lite-beta.js#startOpened=true', function () {
 	Qva.AddDocumentExtension('FixedContent', function () {
 		this.Document.SetOnUpdateComplete(SetFixed);
-	}); 
-//});
+	});
+// });
 
-function SetFixed() { 
+function SetFixed() {
 	tabrowDisplay = $('#Tabrow').css('display');
 	
-	var applyToTabRow = 1;
-	var applyToHeader = 1;
+	$('#QvAjaxToolbar').css("position", "fixed");
+	$("#QvAjaxToolbar").css("top", "0");
+	$("#QvAjaxToolbar").css('z-index', 30000);
 	
-	$( "*[class*=_fixed]" ).each(function() {		
+	$( "*[class*=_fixed]" ).each(function() {
 		$(this).css("position", "fixed");
 		zIndex = $(this).css('z-index');
 		zIndex = parseInt(zIndex);
 		$(this).css('z-index', zIndex + 10000);
+	  
 		if( tabrowDisplay !== 'none') {
-			$(this).css("margin-top", "33px");
+			$(this).css("margin-top", "59px");
+		} else {
+		  $(this).css("margin-top", "25px");
 		}
-	});		
+	
+	  
+	});
 
-	if( applyToTabRow == 1) {
-		if( tabrowDisplay !== 'none') {	
-			$("#Tabrow").css("position", "fixed");
-			$("#Tabrow").css("top", "0");
-			$("#Tabrow").css('z-index', 20000);
-			$("#MainContainer").css("margin-top", "33px");
-		}
+	if( tabrowDisplay !== 'none') {
+		$("#Tabrow").css("position", "fixed");
+	 	$("#Tabrow").css("top", "25px");
+		$("#Tabrow").css('z-index', 20000);
+		$("#MainContainer").css("top", "58px");
+	  $("#MainContainer").css("margin-top", "59px");
 	}
 }
